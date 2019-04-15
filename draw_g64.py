26015b8fc3fbd233de4903fcb536a5f92108a510
Michael Steil <mist64@mac.com>
2019-04-15 12:32:49 +0200

    svg output - too slow to render :(

--- draw_g64.py
+++ draw_g64.py
@@ -19,6 +19,10 @@ size = 5000
 img = Image.new('RGB', (size, size), color = 'white')
 draw = ImageDraw.Draw(img)
 
+print '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">'
+print '<svg width="{}" height="{}">'.format(size, size)
+
+
 for i in range(0, notracks):
 	trackno = i / 2 + 1
 	offset = data[12 + 4 * i] | data[12 + 4 * i + 1] << 8 | data[12 + 4 * i + 2] << 16 | data[12 + 4 * i + 3] << 24
@@ -26,7 +30,7 @@ for i in range(0, notracks):
 		continue
 
 	len = data[offset] | data[offset + 1] << 8
-	print "track {}, offset {}, size {}".format(trackno, offset, len)
+	print "<!-- track {}, offset {}, size {} -->".format(trackno, offset, len)
 
 	sectorlen = len * 8
 	radius = size * .45 - trackno * size / 200
@@ -54,5 +58,8 @@ for i in range(0, notracks):
 #		r = 20
 #		draw.ellipse((x - r, y - r, x + r, y + r), fill = value)
 		draw.line((x1, y1, x2, y2), fill = value)
+		print '<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="#333333" stroke-width="3px"></line>'.format(x1,y1,x2,y2)
 
 img.save(filename_out)
+
+print '</svg>'
