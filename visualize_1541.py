202226a2c579f6be1a9d6299387eeb87d25ea533
Martin Thierer <mthierer@gmail.com>
2020-06-21 16:40:50 +0200

    add python3 compatibility

--- visualize_1541.py
+++ visualize_1541.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function
 import sys, math
 from PIL import Image, ImageDraw
 
@@ -15,10 +16,10 @@ size = 40000
 img = Image.new('RGB', (size, size), color = 'white')
 draw = ImageDraw.Draw(img)
 
-track_distance = size / 150
+track_distance = size // 150
 
 for i in range(0, notracks):
-	trackno = i / 2 + 1
+	trackno = i // 2 + 1
 	offset = data[12 + 4 * i] | data[12 + 4 * i + 1] << 8 | data[12 + 4 * i + 2] << 16 | data[12 + 4 * i + 3] << 24
 	speed = data[0x15c + 4 * i] | data[0x15c + 4 * i + 1] << 8 | data[0x15c + 4 * i + 2] << 16 | data[0x15c + 4 * i + 3] << 24
 	if not offset:
@@ -34,19 +35,19 @@ for i in range(0, notracks):
 	# G64 files built by tools might not contain the tail gap data,
 	# so if there is significantly less data on the track than we expect,
 	# don't scale it to 360 degrees, but leave the tail area empty.
-	sector_capacity = 200000 / [32, 30, 28, 26][speed] * 8
+	sector_capacity = 200000 // [32, 30, 28, 26][speed] * 8
 	if float(sectorlen) / sector_capacity > .993:
 		sector_capacity = sectorlen
 
-	print "track {}, offset {}, size {}, speed {}, capacity {}".format(trackno, offset, len, speed, sector_capacity / 8)
+	print("track {}, offset {}, size {}, speed {}, capacity {}".format(trackno, offset, len, speed, sector_capacity // 8))
 
 	for i in range(0, sectorlen):
 		angle = float(i) / sector_capacity * 2 * math.pi
-		x1 = int(round(size / 2 + radius1 * math.sin(angle)))
-		y1 = int(round(size / 2 + radius1 * math.cos(angle)))
-		x2 = int(round(size / 2 + radius2 * math.sin(angle)))
-		y2 = int(round(size / 2 + radius2 * math.cos(angle)))
-		byte = data[offset + i / 8 + 2]
+		x1 = int(round(size // 2 + radius1 * math.sin(angle)))
+		y1 = int(round(size // 2 + radius1 * math.cos(angle)))
+		x2 = int(round(size // 2 + radius2 * math.sin(angle)))
+		y2 = int(round(size // 2 + radius2 * math.cos(angle)))
+		byte = data[offset + i // 8 + 2]
 		if byte == 0x00:
 			value = (0, 0, 0xff) # empty = blue
 		elif byte == 0xff:
@@ -57,5 +58,5 @@ for i in range(0, notracks):
 
 		draw.line((x1, y1, x2, y2), fill = value, width = 2)
 
-img = img.resize((size / 16, size / 16), Image.ANTIALIAS)
+img = img.resize((size // 16, size // 16), Image.ANTIALIAS)
 img.save(filename_out)
